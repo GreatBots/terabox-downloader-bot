0be5eb5529d3b2960f568d31a7902c104c8e815b
Vivek <vivektp2006@gmail.com>
2024-02-27 11:19:56 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,14 +1,14 @@
-API_ID = 123456  # api id
-API_HASH = "ed44780dedd182084f2133b16944cf565"  # api hash
+API_ID = 2428507  # api id
+API_HASH = "80353e5f8ce893d1366e804c0375c919"  # api hash
 
-BOT_TOKEN = "123456:ABC-DEF1234ghIkl-zyx57W2v1u123ew11"  # bot token
+BOT_TOKEN = "7072330589:AAGX93bmGKrDUbejXJWRAa5zcy5bwwJGlIY"  # bot token
 
 
 ## REDIS
-HOST = "localhost"  # redis host uri
+HOST = "redis-11595.c267.us-east-1-4.ec2.cloud.redislabs.com:11595"  # redis host uri
 PORT = 6379  # redis port
-PASSWORD = ""  # redis password
+PASSWORD = "bd0PaM5F3C9uS44OXUI3hGWDqcDQAPEq"  # redis password
 
-PRIVATE_CHAT_ID = -1001234567890  # CHAT WHERE YOU WANT TO STORE VIDEOS
-COOKIE = ""  # COOKIE FOR AUTHENTICATION (get from chrome dev tools) ex: "PANWEB=1; csrfToken=; lang=en; TSID=; __bid_n=; _ga=; __stripe_mid=; ndus=; browserid==; ndut_fmt=; _ga_06ZNKL8C2E=" (don't use this)
-ADMINS = [1317173146]
+PRIVATE_CHAT_ID = -1001837726299  # CHAT WHERE YOU WANT TO STORE VIDEOS
+COOKIE = "csrfToken=x6--ydMn0oLPFb0RbHq49YOq; browserid=4CgdK7D6HiCT78fsR_XCv_LVjSifrej6Z63k0E952TDu9RmygkoWYLG1cEMzCUbPqemlkS BBYJENYDUP; lang=en; TSID=TP9X7JtM3wCcrwnnompC7miPwLScHlba; _gcl_au=1.1.1425456860.1709008522;_bid_n=18de8d8f53777f64834207; _ga=GA1.1.694972006.1709008624; ndus-Y-jqhKTteHuiWRPqYFGBrsB050Svbhh0zUuWngGK; ndut_fmt=F77E608405379D10A36D9584FEE28DB72B26305F38250DD0435094D0DF4DFDFB; _ga_06ZNKL8C2E=GS1.1.1709008624.1.1.1709008856.60.0.0;"  # COOKIE FOR AUTHENTICATION (get from chrome dev tools) ex: "PANWEB=1; csrfToken=; lang=en; TSID=; __bid_n=; _ga=; __stripe_mid=; ndus=; browserid==; ndut_fmt=; _ga_06ZNKL8C2E=" (don't use this)
+ADMINS = [1496092965]
